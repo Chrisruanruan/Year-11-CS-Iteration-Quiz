3ee80b69cce85166944c64715614c6ce36921f9a
chris <chrisr2025@student.cis.edu.hk>
2023-03-22 08:39:15 +0800

    yay

--- src/Quiz.java
+++ src/Quiz.java
@@ -1,12 +1,26 @@
-public class Quiz {
+import org.w3c.dom.ls.LSOutput;
 
+public class Quiz {
+    public static void main(String[] args) {
+
+        //System.out.println(vowelCount("balobaurseb"));
+        //System.out.println(findN(10));
+        //System.out.println(camelCaseMe("boqa-ba_dsanviu-q"));
+    }
 
 
     /** 1. Write a method that returns the number of vowels in a string.
            Consider a, e, i, o, u as vowels. **/
 
-
-
+    public static int vowelCount (String string){
+        int count = 0;
+        for (int i = 0; i<string.length();i++){
+            if(string.charAt(i) == 'a'||string.charAt(i) == 'e'||string.charAt(i) == 'i'||string.charAt(i) == 'o'||string.charAt(i) == 'u'){
+                count++;
+            }
+        }
+        return count;
+    }
 
 
 
@@ -24,11 +38,21 @@ public class Quiz {
         5 - divides integer by 5
 
         Answer: 1 (only one number doesn't divide integer by any of 2, 3, 5) **/
+    public static int findN(int n){
+        int count = 0;
+        for (int i = 0; i<=n; i++){
+            if (i%2!=0 && i%3!=0 && i%5!=0)count++;
+        }
+        return count;
+    }
 
 
 
 
-    /** 3. Write a method called camelCaseMe so that it converts dash/underscore delimited words into camel casing. The first word within the output should be capitalized only if the original word was capitalized (known as Upper Camel Case, also often referred to as Pascal case). The next words should always be capitalized.
+    /** 3. Write a method called camelCaseMe so that it converts dash/underscore delimited words into camel casing.
+     * The first word within the output should be capitalized only if the original word was capitalized
+     * (known as Upper Camel Case, also often referred to as Pascal case).
+     * The next words should always be capitalized.
 
            Examples
 
@@ -37,6 +61,19 @@ public class Quiz {
            "The_Stealth_Warrior" gets converted to "TheStealthWarrior"
 
            "The_Stealth-Warrior" gets converted to "TheStealthWarrior" **/
+    public static String camelCaseMe (String str){
+        String newString = "";
+        for (int i=0; i<str.length(); i++){
+            if (str.charAt(i)!= '_'&&str.charAt(i)!= '-'){
+                newString = newString + str.charAt(i);
+            }
+            if (str.charAt(i) == '-'){
+                newString = newString + str.substring(i+1,i+2).toUpperCase();
+                i++;
+            }
+        }
+        return newString;
+    }
 
 
 }
